8c5bf70ee89104070e3a1efa22294371480a9472
Marie Akhramenko <triangletrue@yahoo.com>
2020-04-07 17:02:12 +0300

    Add more specs

--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -1,9 +1,23 @@
 require File.expand_path '../spec_helper.rb', __FILE__
 
-describe "Equations Application" do
-  it "should allow accessing the home page" do
-    post '/', {equation: "x-6=0"}.to_json
+describe 'Equations Application' do
+  it 'should be accessible' do
+    post '/', {equation: 'x-6=0'}.to_json
 
     expect(last_response).to be_ok
+  end
+
+  it 'should return error if input is incorrect' do
+    post '/', {equation: 'aaaa'}.to_json
+
+    expect(JSON.parse(last_response.body)['error']).to be true
+  end
+
+  it 'should return amount of roots' do
+    post '/', {equation: 'x^2-4=0'}.to_json
+    roots_amount = JSON.parse(last_response.body).dig('roots', 'roots_amount')
+
+    expect(roots_amount).to be_integer
+    expect(roots_amount).to be <= 2
   end
 end
